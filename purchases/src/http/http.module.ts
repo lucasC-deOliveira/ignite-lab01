8248c214e6405151da2b134b1dae94c443032e66
LucasC-deOliveira <luckericksen@gmail.com>
2022-04-06 12:46:03 -0300

    aula 1 estrutura base dos projetos

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+
+import path from "node:path"
+
+import { DatabaseModule } from 'src/database/database.module';
+import { TestResolver } from 'src/http/test.resolver';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        DatabaseModule,
+        GraphQLModule.forRoot({
+            driver:ApolloDriver,
+            autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+        }),
+    ],
+   providers: [TestResolver]
+})
+export class HttpModule {
+
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# lab 01
+- NestJS
+- GraphQL
+- Apache Krafka
+- Next.js
+- Apollo Client (GraphQL)
+
+
+## Funcionalidades
+
+
+### Serviço de compras (purchases)
+- [Admin] Cadastro de produtos
+- [Admin] Listagem de produtos
+
+- [Auth] Listagem de compras
+
+- [Public] Compra de um produto
+- [Public] Listar produtos disponiveis para compra
+
+### Serviço de sala de aula (classroom)
+- [Admin] Listar mátriculas
+- [Admin] Listar alunos
+- [Admin] Listar cursos
+- [Admin] Cadastrar cursos
+
+- [Auth] Listar cursos que tenha acesso
+- [Auth] Acessar conteudo do curso
